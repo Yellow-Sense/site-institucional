c336ce1e4b8b8630b1cf7de3f6e7f9c486e83927
leandroSalesCotrim <deadpoolleandro@hotmail.com>
2021-09-28 21:01:10 -0300

    Arrumei o botão e organizei o codigo

--- main.js
+++ main.js
@@ -9,7 +9,12 @@ $(window).on("scroll touchmove", function() {
         $('#link_produto').css('color', '#f2f2f2');
         $('#link_contato').css('color', '#f2f2f2');
         $('#link_simulador').css('color', '#f2f2f2');
-
+        $('#btn_entrar_frente').css('background-color', '#FFC200');
+        $('#btn_entrar_fundo').css('background-color', '#F2F2F2');
+        div_menu.style.transition = "0.2s";//Controle do tempo da duração da transição de cor
+        btn_entrar_fundo.style.transition = "0.2s";//Controle do tempo da duração da transição de cor
+        btn_entrar_frente.style.transition = "0.5s";//Controle do tempo da duração da transição de cor
+        
         $('#link_sobre').css('text-decoration','none');
     }
     if ($(document).scrollTop() >= $("#sec_sobre").position().top){
@@ -22,6 +27,12 @@ $(window).on("scroll touchmove", function() {
         $('#link_produto').css('color', '#252525');
         $('#link_contato').css('color', '#252525');
         $('#link_simulador').css('color', '#252525');
+        $('#btn_entrar_frente').css('background-color', '#FFC200');
+        $('#btn_entrar_fundo').css('background-color', '#252525');
+        div_menu.style.transition = "0.2s";//Controle do tempo da duração da transição de cor
+        btn_entrar_fundo.style.transition = "0.2s";//Controle do tempo da duração da transição de cor
+        btn_entrar_frente.style.transition = "0.5s";//Controle do tempo da duração da transição de cor
+
 
         $('#link_inicio').css('text-decoration','none');
     }
