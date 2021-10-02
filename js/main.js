e47ff53c78a815dae3408c53e3092d5939816d31
leandroSalesCotrim <deadpoolleandro@hotmail.com>
2021-10-01 23:09:02 -0300

    Grandes alterações de CSS: o CSS de todas as seções foi alterado e agora está mais responsivo com o conteudo da ultima aula do rafa, o HTML também foi alterado para ficar melhor de mexer com as div, e alterei a posição dos links do menu, resolvido problema da seção sobre nós ficar aparecendo uma margem branca do fundo

--- js/main.js
+++ js/main.js
@@ -5,41 +5,41 @@ $(window).on("scroll touchmove", function() {
     //Função que se o scrollTop da pagina estiver na seção inicio, altera o css da pagina
     if ($(document).scrollTop() >= $("#sec_inicio").position().top){
         $('#div_menu').css('background-color', 'rgba(0, 0, 0, 0)');
-        $('#link_inicio').css('text-decoration','underline');
-        $('#link_inicio').css('text-decoration-color','#ffc200');
+        $('.link_inicio').css('text-decoration','underline');
+        $('.link_inicio').css('text-decoration-color','#ffc200');
         
-        $('#link_inicio').css('color', '#f2f2f2');
-        $('#link_sobre').css('color', '#f2f2f2');
-        $('#link_produto').css('color', '#f2f2f2');
-        $('#link_contato').css('color', '#f2f2f2');
-        $('#link_simulador').css('color', '#f2f2f2');
-        $('#btn_entrar_frente').css('background-color', '#FFC200');
-        $('#btn_entrar_fundo').css('background-color', '#F2F2F2');
+        $('.link_inicio').css('color', '#f2f2f2');
+        $('.link_sobre').css('color', '#f2f2f2');
+        $('.link_produto').css('color', '#f2f2f2');
+        $('.link_contato').css('color', '#f2f2f2');
+        $('.link_simulador').css('color', '#f2f2f2');
+        $('#btn_entrar').css('background-color', '#FFC200');
+        $('#fundo_btn_entrar').css('background-color', '#F2F2F2');
         div_menu.style.transition = "0.2s";//Controle do tempo da duração da transição de cor
-        btn_entrar_fundo.style.transition = "0.2s";//Controle do tempo da duração da transição de cor
-        btn_entrar_frente.style.transition = "0.5s";//Controle do tempo da duração da transição de cor
+        fundo_btn_entrar.style.transition = "0.2s";//Controle do tempo da duração da transição de cor
+        btn_entrar.style.transition = "0.5s";//Controle do tempo da duração da transição de cor
         
-        $('#link_sobre').css('text-decoration','none');
+        $('.link_sobre').css('text-decoration','none');
     }
 
     //Função que se o scrollTop da pagina estiver na seção Sobre, altera o css da pagina
     if ($(document).scrollTop() >= $("#sec_sobre").position().top){
         $('#div_menu').css('background-color', '#f2f2f2');
-        $('#link_sobre').css('text-decoration','underline');
-        $('#link_sobre').css('text-decoration-color','#ffc200');
+        $('.link_sobre').css('text-decoration','underline');
+        $('.link_sobre').css('text-decoration-color','#ffc200');
 
-        $('#link_inicio').css('color', '#252525');
-        $('#link_sobre').css('color', '#252525');
-        $('#link_produto').css('color', '#252525');
-        $('#link_contato').css('color', '#252525');
-        $('#link_simulador').css('color', '#252525');
-        $('#btn_entrar_frente').css('background-color', '#FFC200');
-        $('#btn_entrar_fundo').css('background-color', '#252525');
+        $('.link_inicio').css('color', '#252525');
+        $('.link_sobre').css('color', '#252525');
+        $('.link_produto').css('color', '#252525');
+        $('.link_contato').css('color', '#252525');
+        $('.link_simulador').css('color', '#252525');
+        $('#btn_entrar').css('background-color', '#FFC200');
+        $('#fundo_btn_entrar').css('background-color', '#252525');
         div_menu.style.transition = "0.2s";//Controle do tempo da duração da transição de cor
-        btn_entrar_fundo.style.transition = "0.2s";//Controle do tempo da duração da transição de cor
-        btn_entrar_frente.style.transition = "0.5s";//Controle do tempo da duração da transição de cor
+        fundo_btn_entrar.style.transition = "0.2s";//Controle do tempo da duração da transição de cor
+        btn_entrar.style.transition = "0.5s";//Controle do tempo da duração da transição de cor
 
 
-        $('#link_inicio').css('text-decoration','none');
+        $('.link_inicio').css('text-decoration','none');
     }
 });
