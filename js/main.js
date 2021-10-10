257c52fba02397408328a39eeeb007fde0a14b2f
matheusaraujocf <MATHEUS.ACARVALHO@bandtec.com.br>
2021-10-10 15:26:45 -0300

    adicionei o simulaador nos arquivos html, css e js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,40 @@
+function calculo(){
+    var qtde_ovo       = Number(qtd_ovo.value);
+    var qtde_perdida   = Number(qtd_perda.value);
+    var valor_vendido  = Number(qtd_preco.value);
+    var area_granja    = Number(qtd_area.value);
+    var qtde_sensor    = (area_granja / 10);
+
+    var lucro_solo = ((qtde_ovo - (qtde_ovo * (qtde_perdida / 100))) * valor_vendido).toFixed(2);
+    var lucro_projeto = ((qtde_ovo - (qtde_ovo * (10/ 100))) * valor_vendido).toFixed(2);
+    var potencial = (90 -(100 - qtde_perdida)).toFixed(2);
+    var ano = ((lucro_projeto - lucro_solo) * 4 * 12).toFixed(2);
+    var mes = ((lucro_projeto - lucro_solo) * 4 ).toFixed(2);
+    var sensor = (1  * qtde_sensor);
+    var instalacao = qtde_sensor * 80;
+    var total = (sensor + instalacao);
+
+    resultado.innerHTML = `
+    <h2 class="h2-js"> Potencial de ganhos </h2>
+
+        <b class="b-js"> Sem o <span class="span-yellow">Yellow Sensor</span>,você teve um lucro de aproximadamente R$ ${lucro_solo}. <br>
+
+        <b class="b-js">Com a nossa ajuda <span class="span-yellow"> Yellow Sensor</span> você poderá obter um lucro de aproximadente R$ ${lucro_projeto}.</b> <br>
+
+        <b class="b-js">O <span class="span-yellow">Yellow Sensor</span> teria o potencial de lucrar aproximadamente ${potencial}% a mais. <br>
+
+    <h2 class="h2-js">Diferença de lucro com o projeto</h2>
+
+        <b class="b-js"> Rendendo um lucro a mais de aproximadamente R$ ${mes} ao mês. </b> <br>
+
+        <b class="b-js"> Rendendo um lucro a mais de aproximadamente R$ ${ano} ao ano. </b> <br>
+
+        <h2 class="h2-js">Orçamento</h2>
+
+        <b class="b-js"> O valor do projeto com a instalação seria em torno de R$${total} </b> <br>     
+
+    `
+}
 
 //Função que verifica a posição do scrool ao ser movimentado
 $(window).on("scroll touchmove", function() {
